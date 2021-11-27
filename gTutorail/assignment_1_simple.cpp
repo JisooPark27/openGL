62b9fc405af9959485a0c98cd5efced3c84b15f7
JisooPark27 <jspark9812@gmail.com>
2021-11-27 22:24:59 +0900

    assignment1

---
+++ gTutorail/assignment_1_simple.cpp
@@ -0,0 +1,21 @@
+﻿#include <GL/freeglut.h>
+
+void myDisplay() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_QUADS);
+
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(-0.5, 0.5);
+	glVertex2f(0.5, 0.5);
+	glVertex2f(0.5, -0.5);
+
+	glEnd();
+	glFlush();
+}
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutCreateWindow("[과제1]17101212박지수");
+	glutDisplayFunc(myDisplay);
+	glutMainLoop();
+}
